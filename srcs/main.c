0792fd0abf274f26a65ed54877abd51b7657e7f7
Carolina SOMARRIBA <casomarr@made-f0Cr6s3.clusters.42paris.fr>
2023-12-11 15:41:07 +0100

    caro : pull main

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,7 @@
 /*   By: casomarr <casomarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 12:44:16 by amugnier          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/12/11 14:07:41 by casomarr         ###   ########.fr       */
-=======
-/*   Updated: 2023/12/11 15:13:15 by amugnier         ###   ########.fr       */
->>>>>>> main
+/*   Updated: 2023/12/11 15:40:11 by casomarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +14,9 @@
 
 int	main(int argc, char **argv)
 {
-<<<<<<< HEAD
-	void	*mlx_ptr;
-	void	*win_ptr;
-
-	printf("Hello World CARO!\n");
-	mlx_ptr = mlx_init();
-	if (!mlx_ptr)
-=======
 	if (argc != 2)
 	{
 		printf("Error\nUsage ./miniRT <file.rt>\n");
->>>>>>> main
 		return (0);
 	}
 	if (parsing(argv[1]) == false)
